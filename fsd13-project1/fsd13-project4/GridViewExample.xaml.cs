e2fa85be24ef4ff78d70dbaf53bdb57826896d87
rezash86 <reza.sh@gmail.com>
2025-07-07 00:53:13 -0400

    adding grid and gridview

---
+++ fsd13-project1/fsd13-project4/GridViewExample.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace fsd13_project4
+{
+    /// <summary>
+    /// Interaction logic for GridViewExample.xaml
+    /// </summary>
+    public partial class GridViewExample : Window
+    {
+        
+        public List<Person> People { get; set; }
+        public GridViewExample()
+        {
+            InitializeComponent();
+            People = new List<Person>
+            {
+                new Person {Name = "Alex", Age= 20},
+                new Person{ Name= "Rose", Age = 18}
+            };
+
+            //lvPoepleData.ItemsSource = People;
+            DataContext = this;
+        }
+    }
+}
