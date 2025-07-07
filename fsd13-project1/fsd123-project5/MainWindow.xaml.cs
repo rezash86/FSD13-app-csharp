6394b19eadfed98488b376a41b069d38b804b322
rezash86 <reza.sh@gmail.com>
2025-07-07 13:29:41 -0400

    2 window comminucations

---
+++ fsd13-project1/fsd123-project5/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace fsd123_project5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnSandwichSelector_Click(object sender, RoutedEventArgs e)
+        {
+            SanwichMakerWindow sanwichMakerWindow = new SanwichMakerWindow();
+            sanwichMakerWindow.Owner = this;
+
+            //It will take some values and bring them here
+            //Subscribe to the event in the sandwich maker
+            //when the sandwichmaker window calls .Invoke(..) this annynomus function runs
+
+            sanwichMakerWindow.AssignResult += (br, veg, meat) => {
+                lblBread.Content = br;
+                lblVeggies.Content = veg;
+                lblMeat.Content = meat;
+            }; ;
+
+
+            bool? result = sanwichMakerWindow.ShowDialog();
+        }
+
+        //private void getInfo(string br, string veggies, string meat)
+        //{
+        //    lblBread.Content = br.ToString();
+        //    lblVeggies.Content = veggies.ToString();
+        //    lblMeat.Content = meat.ToString();
+        //}
+    }
+}
---
+++ fsd13-project1/fsd123-project5/SanwichMakerWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace fsd123_project5
+{
+    /// <summary>
+    /// Interaction logic for SanwichMakerWindow.xaml
+    /// </summary>
+    public partial class SanwichMakerWindow : Window
+    {
+        //this defines an event that any other class can SUBSCRIBE to
+        // "Hey. I am ready to announce a new sandwich !
+        public event Action<string, string, string> AssignResult;
+        
+        public SanwichMakerWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSaveInfo_Click(object sender, RoutedEventArgs e)
+        {
+            string breadType = comboBreads.Text;
+            List<string> selectedVeggies = new List<string>();
+            if (chbxLettuce.IsChecked == true)
+            {
+                selectedVeggies.Add("Lettuce");
+            }
+            if (chbxTomato.IsChecked == true)
+            {
+                selectedVeggies.Add("Tomato");
+            }
+            if (chbxCucumber.IsChecked == true)
+            {
+                selectedVeggies.Add("cucumber");
+            }
+
+            string meat = "";
+            if (rbtnChicken.IsChecked == true)
+            {
+                meat = "chicken";
+            }
+            if (rbtnTofu.IsChecked == true)
+            {
+                meat = "Tofu";
+            }
+            if (rbtnTurkey.IsChecked == true)
+            {
+                meat = "Turkey";
+            }
+
+            //Raise a trigger to the event that others want to subscribe
+            AssignResult?.Invoke(breadType, string.Join(",", selectedVeggies), meat);
+
+            DialogResult = true; // closes the dialog and return true;
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
